80eb5ce5d0be10535bf8ced45cb9205e363b50da
TonyGalvan <tonygalvan010@gmail.com>
2020-10-08 01:28:12 -0500

    tarea menu completo

--- src/uaslp/enginering/labs/Checkbox.java
+++ src/uaslp/enginering/labs/Checkbox.java
@@ -1,4 +1,13 @@
 package uaslp.enginering.labs;
 
-public class Checkbox {
+public class Checkbox extends Control {
+    private String label;
+
+    public Checkbox(String label){
+        this.label = label;
+    }
+    public  void draw(){
+        System.out.print("[ ] " + label);
+    }
+
 }
